4d5db90c2b58435118439abe861e77e855cf912d
BlackBorada <hab.osa@mail.ru>
2024-03-26 03:53:11 +0500

    Init tests

---
+++ tests/test_circle.py
@@ -0,0 +1,16 @@
+import unittest
+from math import pi
+from ..circle import Circle
+
+class TestCircle(unittest.TestCase):
+    def test_valid_circle(self):
+        pass
+
+    def test_invalid_radius_type(self):
+        pass
+
+    def test_invalid_radious_value(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_triangle.py
@@ -0,0 +1,14 @@
+import unittest
+from ..triangel import Triangle
+
+class TestTriangle(unittest.TestCase):
+    def test_valid_triangle(self):
+        pass
+
+    def test_ivalid_sides(self):
+        pass
+
+
+
+if __name__ == '__main__':
+    unittest.main()
