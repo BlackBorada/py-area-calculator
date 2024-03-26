48c0f9ee89bec0910fd303cf06f88821e37b0b00
BlackBorada <hab.osa@mail.ru>
2024-03-26 10:51:50 +0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
 from .figure import Figure
 from .circle import Circle
-
+from .triangel import Triangle
 __version__ = "0.0.1"
 
-__all__ = ["Figure", "Circle"]
+__all__ = ["Figure", "Circle", "Triangle"]
 
 """
 Библеотека для вычисления площяди фигур.
