ce86096ede37fbace53e857d9948a0bb6e6ff030
BlackBorada <hab.osa@mail.ru>
2024-03-26 13:18:47 +0500

    исправление опечаток

--- __init__.py
+++ __init__.py
@@ -1,10 +1,12 @@
 from .figure import Figure
 from .circle import Circle
-from .triangel import Triangle
+from .triangle import Triangle
+
+
 __version__ = "0.0.1"
 
 __all__ = ["Figure", "Circle", "Triangle"]
 
 """
-Библеотека для вычисления площяди фигур.
+Библиотека для вычисления площади фигур.
 """
